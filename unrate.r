d1d794a75a7397ef17365649205e04a39caa3d7d
Rob Hanssen <rob.hanssen@outlook.com>
2022-07-07 10:02:08 -0400

    axis label change

--- unrate.r
+++ unrate.r
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(patchwork)
 
 source("functions.r")
 
@@ -15,18 +16,18 @@ unrate2003m <-
     mutate(time = (date - ymd(20080101)) / dmonths(1)) %>%
     filter(time > 0)
 
-unrate_nls3 <-
+unrate_nls2008 <-
     nls(value ~ a0 + a1 * exp(-1 / a3 * (log(time) - a2)^2),
         start = list(a0 = 4, a1 = 6, a2 = 3.5, a3 = 1),
         data = unrate2003m
     )
 
-model_2003 <- broom::augment(unrate_nls3,
-    newdata = tibble(time = 1:(15 * 12))
+model_2003 <- broom::augment(unrate_nls2008,
+    newdata = tibble(time = 1:(15 * 22))
 ) %>%
     mutate(date = ymd(20080101) + months(time))
 
-broom::tidy(unrate_nls3)
+broom::tidy(unrate_nls2008)
 
 #
 # monthly unemployment fitting in 2000-2005
@@ -38,15 +39,15 @@ unrate200m <-
     filter(time > 0)
 
 
-unrate_nls4 <-
+unrate_nls2000 <-
     nls(value ~ a0 + a1 * exp(-1 / a3 * (log(time) - a2)^2),
         start = list(a0 = 4, a1 = 6, a2 = 3.5, a3 = 1),
         data = unrate200m
     )
 
-broom::tidy(unrate_nls4)
+broom::tidy(unrate_nls2000)
 
-model_200 <- broom::augment(unrate_nls4,
+model_200 <- broom::augment(unrate_nls2000,
     newdata = tibble(time = 1:(12 * 12))
 ) %>%
     mutate(date = ymd(20000101) + months(time))
@@ -62,27 +63,83 @@ unrate1990 <-
     mutate(time = (date - ymd(19900101)) / dmonths(1)) %>%
     filter(time > 0)
 
-unrate_nls5 <-
+unrate_nls1990 <-
     nls(value ~ a0 + a1 * exp(-1 / a3 * (log(time) - a2)^2),
         start = list(a0 = 4, a1 = 6, a2 = 3.5, a3 = 1),
         data = unrate1990
     )
 
-model_1990 <- broom::augment(unrate_nls5,
+model_1990 <- broom::augment(unrate_nls1990,
+    newdata = tibble(time = 1:(22 * 12))
+) %>%
+    mutate(date = ymd(19900101) + months(time))
+
+broom::tidy(unrate_nls1990)
+
+#
+# monthly unemployment fitting in 1990-2000
+#
+
+
+unrate1990a <-
+    unrate %>%
+    filter(date > ymd(19900101), date < ymd(20000101)) %>%
+    mutate(time = (date - ymd(19900101)) / dmonths(1)) %>%
+    filter(time > 0)
+
+unrate_1990a <-
+    nls(value ~ a0 + a1 * exp(-1 / a3 * (log(time) - a2)^2),
+        start = list(a0 = 4, a1 = 6, a2 = 3.5, a3 = 1),
+        data = unrate1990a
+    )
+
+model_1990a <- broom::augment(unrate_1990a,
     newdata = tibble(time = 1:(12 * 12))
 ) %>%
     mutate(date = ymd(19900101) + months(time))
 
-broom::tidy(unrate_nls5)
+broom::tidy(unrate_1990a)
 
 #
 # FULL GRAPHS
 #
 
-
-ggplot(data = unrate %>% filter(date > ymd(19900101))) +
+unemp <-
+    ggplot(data = unrate %>% filter(date > ymd(19900101))) +
     aes(date, value) +
     geom_point(aes(y = value), alpha = .1) +
     geom_line(data = model_200, aes(y = .fitted), lty = 2) +
     geom_line(data = model_2003, aes(y = .fitted), lty = 2) +
-    geom_line(data = model_1990, aes(y = .fitted), lty = 2)
+    geom_line(data = model_1990a, aes(y = .fitted), lty = 2) + 
+    scale_y_continuous(limits = c(0, NA)) + 
+    labs(x = "Date",
+        y = "Unemployment rate (in %)") +
+    theme_light()
+
+#
+# model conclusions
+#
+
+extract_value <- function(mod) {
+    mod %>% 
+        broom::tidy() %>% 
+        filter(term == "a0") %>% pull(estimate)
+}
+
+sumry <- tibble(
+            year = c("2010-2020", "2000-2005", "1990-1995"),
+            limit_value = purrr::map_dbl(list(unrate_nls2008, unrate_nls2000, unrate_1990a), ~extract_value(.x))
+    )
+
+limit <-
+    sumry %>% 
+    ggplot +
+    aes(factor(year), limit_value) +
+    geom_col() + 
+    theme_light() + 
+    labs(x = "Time period",
+         y = "Unemployment limit value (in %)")
+
+p <- unemp + limit
+
+ggsave("graphs/unemployment-analysis.png", plot = p, width = 12, height = 6)
