a287e84d2ec90179e89b13f75bd558f3cf6d690f
Rob Hanssen <rob.hanssen@outlook.com>
2022-07-07 09:30:58 -0400

    updates in models and graphs

--- unrate.r
+++ unrate.r
@@ -5,62 +5,84 @@ source("functions.r")
 
 unrate <- retrieve_data("UNRATE", "FRED")
 
-ggplot(unrate %>% filter(date > ymd(20120101))) +
-  aes(date, value) + 
-  geom_point()
+#
+# monthly unemployment fitting since 2008
+#
 
-
-ggplot(unrate) +
-  aes(date, value) + 
-  geom_point()
-
-
-unrate2010 <-
-    unrate %>% filter(date > ymd(20100101), date < ymd(20200101))
-
-unrate_mod <- lm(log10(value) ~ date, data = unrate2010)
-
-broom::augment(unrate_mod, newdata = tibble(date = seq(ymd(20100101), today(), by = "month"))) %>%
-    mutate(.fitted = 10^.fitted) %>%
-    ggplot + aes(date, .fitted) + geom_line() + 
-    geom_point(data = unrate2010, aes(y = value))
-
-unrate2010_undated <- 
-    unrate2010 %>% mutate(time = (date - ymd(20100101))/dyears(1))
-
-unrate_nls <- 
-    nls(value ~ a0 + a1 * 10^(-time / a2), start = list(a0 = 2, a1 = 5, a2 = 5), data = unrate2010_undated)
-
-
-broom::augment(unrate_nls, newdata = tibble(time = 1:200)) %>%
-    mutate(date = ymd(20100110) + years(time)) %>%
-    # mutate(.fitted = 10^.fitted) %>%
-    ggplot + aes(date, .fitted) + geom_line() + 
-    geom_point(data = unrate %>% filter(date > ymd(20000101)), aes(y = value))
-
-
-
-
-
-unrate2003 <-
-    unrate %>% filter(date > ymd(20030101), date < ymd(20200101)) %>%
-    mutate(time = (date - ymd(20070101))/dyears(1)) %>%
+unrate2003m <-
+    unrate %>%
+    filter(date > ymd(20030101), date < ymd(20200101)) %>%
+    mutate(time = (date - ymd(20080101)) / dmonths(1)) %>%
     filter(time > 0)
 
-# unrate2003 %>%
-#     ggplot +
-#   aes(date, value) + 
-#   geom_point()
+unrate_nls3 <-
+    nls(value ~ a0 + a1 * exp(-1 / a3 * (log(time) - a2)^2),
+        start = list(a0 = 4, a1 = 6, a2 = 3.5, a3 = 1),
+        data = unrate2003m
+    )
 
-# write_csv(unrate2003, "uni2.csv")
+model_2003 <- broom::augment(unrate_nls3,
+    newdata = tibble(time = 1:(15 * 12))
+) %>%
+    mutate(date = ymd(20080101) + months(time))
 
-unrate_nls2 <- 
-    nls(value ~ a0+ a1 * exp(-1/a3*(log(time) - a2)^2), start = list(a0 = 4, a1 = 15, a2 = 1, a3=1 ), data = unrate2003)
+broom::tidy(unrate_nls3)
 
-broom::augment(unrate_nls2, newdata = tibble(time = 1:15)) %>%
-    mutate(date = ymd(20070101) + years(time)) %>%
-    # mutate(.fitted = 10^.fitted) %>%
-    ggplot + aes(date, .fitted) + geom_line() + 
-    geom_point(data = unrate %>% filter(date > ymd(20000101)), aes(y = value))
+#
+# monthly unemployment fitting in 2000-2005
+#
+unrate200m <-
+    unrate %>%
+    filter(date > ymd(20000101), date < ymd(20060101)) %>%
+    mutate(time = (date - ymd(20000101)) / dmonths(1)) %>%
+    filter(time > 0)
 
-broom::tidy(unrate_nls2)
+
+unrate_nls4 <-
+    nls(value ~ a0 + a1 * exp(-1 / a3 * (log(time) - a2)^2),
+        start = list(a0 = 4, a1 = 6, a2 = 3.5, a3 = 1),
+        data = unrate200m
+    )
+
+broom::tidy(unrate_nls4)
+
+model_200 <- broom::augment(unrate_nls4,
+    newdata = tibble(time = 1:(12 * 12))
+) %>%
+    mutate(date = ymd(20000101) + months(time))
+
+
+#
+# monthly unemployment fitting in 1990-1995
+#
+
+unrate1990 <-
+    unrate %>%
+    filter(date > ymd(19900101), date < ymd(19950101)) %>%
+    mutate(time = (date - ymd(19900101)) / dmonths(1)) %>%
+    filter(time > 0)
+
+unrate_nls5 <-
+    nls(value ~ a0 + a1 * exp(-1 / a3 * (log(time) - a2)^2),
+        start = list(a0 = 4, a1 = 6, a2 = 3.5, a3 = 1),
+        data = unrate1990
+    )
+
+model_1990 <- broom::augment(unrate_nls5,
+    newdata = tibble(time = 1:(12 * 12))
+) %>%
+    mutate(date = ymd(19900101) + months(time))
+
+broom::tidy(unrate_nls5)
+
+#
+# FULL GRAPHS
+#
+
+
+ggplot(data = unrate %>% filter(date > ymd(19900101))) +
+    aes(date, value) +
+    geom_point(aes(y = value), alpha = .1) +
+    geom_line(data = model_200, aes(y = .fitted), lty = 2) +
+    geom_line(data = model_2003, aes(y = .fitted), lty = 2) +
+    geom_line(data = model_1990, aes(y = .fitted), lty = 2)
