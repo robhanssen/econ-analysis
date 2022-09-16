8b07c04a64e9ef176e0c9aebe5d552a66acdca64
Rob Hanssen <rob.hanssen@outlook.com>
2022-09-16 14:22:23 -0400

    final predictive graph

--- gas-vs-fuel.r
+++ gas-vs-fuel.r
@@ -20,10 +20,10 @@ oilfuel <-
 decade_length <- 5
 max_year <- max(year(oilfuel$date))
 
-oilfuel %>%
-    mutate(dec = 5*(year(date) %/% 5), dec = factor(dec)) %>%
-    ggplot + aes(oilprice, fuelprice, color = dec) + geom_point() + 
-    geom_smooth(method = "lm", se = F)
+# oilfuel %>%
+#     mutate(dec = 5*(year(date) %/% 5), dec = factor(dec)) %>%
+#     ggplot + aes(oilprice, fuelprice, color = dec) + geom_point() + 
+#     geom_smooth(method = "lm", se = F)
 
 
 models <-
@@ -81,3 +81,26 @@ linearanalysis %>%
     unnest(preddate) %>%
     filter(oilprice > 0 ) %>%
     ggplot + aes(oilprice, .fitted, color = halfdecade) + geom_line()
+
+
+preddate <- tail(oilfuel %>% arrange(date),1)
+
+oilbase = scales::dollar(preddate$oilprice * 42)
+fuelbase = scales::dollar(preddate$fuelprice)
+
+linearanalysis %>%
+    filter(term == "oilprice") %>%
+    select(halfdecade, data, models) %>%
+    mutate(predictions = map(models, ~broom::augment(.x, newdata = preddate, interval = "prediction"))) %>%
+    unnest(predictions) %>%
+    select(halfdecade, .fitted, .lower, .upper) %>%
+    ggplot + aes(y = halfdecade, x = .fitted) + geom_point() + geom_vline(xintercept = preddate$fuelprice, lty = 2) + 
+    geom_errorbar(aes(xmin = .lower, xmax = .upper), width = .2) + 
+    annotate("label", preddate$fuelprice, 1, label = glue::glue("Current\n{fuelbase} / gal")) +
+    scale_x_continuous(labels = scales::dollar_format()) +
+    labs(x = "Predicted fuel price (in $/gallon)",
+         y = NULL,
+         title = glue::glue("What would fuel prices have been with oil at {oilbase} per barrel over the 30 years?"))
+
+
+ggsave("graphs/fuelprice_prediction.png", width = 8, height = 6)
