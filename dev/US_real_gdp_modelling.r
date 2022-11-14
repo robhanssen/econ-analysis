612c31c0ced5b69d435f3067cabe19e69b61091c
Rob Hanssen <rob.hanssen@outlook.com>
2022-11-14 14:21:09 -0500

    real gdp models since 1960

---
+++ dev/US_real_gdp_modelling.r
@@ -0,0 +1,151 @@
+library(tidyverse)
+library(lubridate)
+library(broom)
+library(patchwork)
+theme_set(theme_light())
+
+source("functions.r")
+
+gdp <- retrieve_data("GDPC1", "FRED")
+
+gdp_plot <-
+    gdp %>%
+    mutate(floordate = floor_date(date, unit = "quarter")) %>%
+    left_join(presidentinfo, by = "floordate") %>%
+    fill(party, .direction = "down")
+
+
+gdp_plot %>%
+    # filter(date > ymd("1979-12-31")) %>%
+    ggplot() +
+    aes(date, value, color = party, group = TRUE) +
+    geom_line() +
+    scale_y_log10(
+        breaks = c(1, 2, 5) * 10^rep(1:10, each = 3),
+        limits = c(2000, NA),
+        labels = scales::dollar_format(accurary = 1)
+    ) +
+    geom_vline(xintercept = inaugdates, lty = 2, alpha = .3) +
+    theme_light() +
+    labs(
+        x = "Date",
+        y = "US GDP (in billions of USD)",
+        caption = "Dashed vertical line indicate inauguration dates",
+        title = "US GDP since 1980"
+    ) +
+    scale_color_manual(values = partycolor) +
+    theme(legend.position = "none")
+
+
+date_cuts <-
+    c(ymd(19000101), 
+    ymd(19600101), 
+    ymd(19700101), 
+    ymd(19910101), 
+    ymd(20000101),
+    ymd(20020101), 
+    ymd(20070101), 
+    ymd(20090101), 
+    today(), 
+    today() + years(50)
+    )
+
+
+
+models <- gdp %>%
+    mutate(period = cut(date, breaks = date_cuts)) %>% 
+    group_by(period) %>%
+    nest() %>%
+    mutate(gdp_model = map(data, function(tbl) lm(log10(value) ~ date, data = tbl))) %>%
+    filter(!period %in% c("1900-01-01", "2007-01-01", "2000-01-01"))
+
+
+models %>%
+    mutate(params = map(gdp_model, ~broom::tidy(.x))) %>% 
+    unnest(params) %>%
+    filter(term == "date") %>%
+    mutate(growth = 10 ^ (estimate * 365) - 1)
+
+
+# extrapolate
+
+futuredata <- tibble(date = seq(date_cuts[2],
+    date_cuts[length(date_cuts) - 1],
+    by = "month"
+))
+
+gdp_data <-
+    gdp %>%
+    mutate(period = cut(date, breaks = date_cuts))
+
+
+
+models %>%
+    mutate(extradata = map(gdp_model, ~broom::augment(.x, newdata = futuredata))) %>%
+    unnest(extradata) %>%
+    mutate(gdp_real = 10^(.fitted)) %>%
+    ggplot + aes(x =date, y = gdp_real, color = period) + 
+    geom_line(show.legend = FALSE) + 
+    geom_point(data = gdp_data, aes(x = date, y = value, color = period), inherit.aes = FALSE, show.legend = FALSE) +
+    scale_x_date(limits = c(ymd(19600101), NA)) + 
+    scale_y_log10(limits = c(1000, 27000))
+
+
+growth_models <-
+    models %>%
+    mutate(params = map(gdp_model, ~broom::tidy(.x))) %>% 
+    unnest(params) %>%
+    filter(term == "date") %>%
+    mutate(growth = 10 ^ (estimate * 365) - 1)
+
+growth_models %>%
+    ggplot(aes(period, growth)) + geom_col() +
+    scale_y_continuous(labels = scales::label_percent())
+
+label = tibble(date = ymd(paste(growth_models$period)),
+        value = growth_models$growth) %>%
+        mutate(growth = scales::percent(value))
+
+
+models %>%
+    mutate(extradata = map(gdp_model, ~broom::augment(.x, newdata = futuredata))) %>%
+    unnest(extradata) %>%
+    mutate(gdp_real = 10^(.fitted)) %>%
+    mutate(period2 = cut(date, breaks = date_cuts)) %>%
+    filter(period == period2) %>%
+    left_join(gdp_data, by = c("date", "period")) %>%
+    mutate(model_diff = (value - gdp_real)/value) %>%
+    drop_na() %>%
+    ggplot + aes(x =date, y = model_diff, color = period) + 
+    geom_line(show.legend = FALSE) + 
+    scale_y_continuous(labels = scales::label_percent())
+
+
+models %>%
+    mutate(extradata = map(gdp_model, ~broom::augment(.x, newdata = futuredata))) %>%
+    unnest(extradata) %>%
+    mutate(gdp_real = 10^(.fitted)) %>%
+    mutate(period2 = cut(date, breaks = date_cuts)) %>%
+    filter(period == period2) %>%
+    left_join(gdp_data, by = c("date", "period")) %>%
+    mutate(model_diff = value - gdp_real) %>%
+    drop_na() %>%
+    ggplot + aes(x =date, y = model_diff, color = period) + 
+    geom_line(show.legend = FALSE) #+ 
+    # geom_line(aes(y = gdp_real))
+    # scale_y_continuous(labels = scales::label_percent())
+
+models %>%
+    mutate(extradata = map(gdp_model, ~broom::augment(.x, newdata = futuredata))) %>%
+    unnest(extradata) %>%
+    mutate(gdp_real = 10^(.fitted)) %>%
+    mutate(period2 = cut(date, breaks = date_cuts)) %>%
+    filter(period == period2) %>%
+    left_join(gdp_data, by = c("date", "period")) %>%
+    mutate(model_diff = value - gdp_real) %>%
+    drop_na() %>%
+    ggplot + aes(x =date, y = value, color = period) + 
+    geom_line(aes(y = gdp_real)) +
+    geom_point(show.legend = FALSE, alpha = .3) +
+    geom_label(data = label, aes(x = date + years(4), y = 1000, label = growth, fill = factor(date)), hjust = 0, inherit.aes = FALSE) +
+    theme(legend.position = "none")
