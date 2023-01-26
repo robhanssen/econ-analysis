5a4aab43c696b0be2b50478ed4c664148fb1f1a0
Rob Hanssen <rob.hanssen@outlook.com>
2023-01-26 12:49:20 -0500

    change in how joins and data input are done

--- scripts/debt_gdpratio.r
+++ scripts/debt_gdpratio.r
@@ -4,24 +4,27 @@ theme_set(theme_light())
 
 source("functions.r")
 
-debt <- get_index("GFDEBTN")
-gdp <- get_index("GDP")
+debt_gdp <-
+    retrieve_data(indexes = c("GDP", "GFDEBTN"), "FRED") %>%
+    pivot_wider(names_from = "index") %>%
+    arrange(date) %>%
+    fill(GFDEBTN, .direction = "down") %>%
+    drop_na() %>%
+    mutate(gdp_debt_ratio = GFDEBTN / GDP / 1000)
 
 debt_gdp_ratio <-
-    inner_join(debt, gdp, by = "date") %>%
-    mutate(
-        gdp_debt_ratio = value.x / value.y / 1000,
-        floordate = floor_date(date, unit = "quarter")
-    ) %>%
-    left_join(presidentinfo, by = "floordate") %>%
-    fill(president, .direction = "down") %>%
-    fill(party, .direction = "down")
+    full_join(presidentinfo, debt_gdp, by = c("inaugdate" = "date")) %>%
+    arrange(inaugdate) %>%
+    fill(c("president", "party"), .direction = "down") %>%
+    select(-floordate) %>%
+    rename(date = inaugdate) %>%
+    drop_na()
 
 debt_gdp_ratio %>%
     filter(date > ymd("1979-12-31")) %>%
     ggplot() +
     aes(date, gdp_debt_ratio, color = party, group = FALSE) +
-    geom_line() +
+    geom_line(show.legend = FALSE) +
     scale_y_continuous(
         breaks = .2 * 0:100,
         limits = c(0, NA),
@@ -36,9 +39,15 @@ debt_gdp_ratio %>%
         title = "Ratio of Public Debt to GDP since 1980"
     ) +
     scale_color_manual(values = partycolor) +
-    theme(legend.position = "none")
+    theme(
+        axis.ticks = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title.position = "plot",
+        plot.caption = element_text(hjust = 0),
+        plot.caption.position = "plot"
+    )
 
 ggsave("graphs/debt-to-gdp-ratio-since-1980.png",
     width = 6,
-    height = 6
+    height = 4
 )
