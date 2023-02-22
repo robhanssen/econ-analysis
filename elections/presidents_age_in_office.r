3e51652874d19dad05b3768b9b82b3fed6dfcd66
Rob Hanssen <rob.hanssen@outlook.com>
2023-02-22 11:27:23 -0500

    clean-up

--- elections/presidents_age_in_office.r
+++ elections/presidents_age_in_office.r
@@ -11,41 +11,72 @@ data_raw <- url %>%
     html_table(fill = TRUE) %>%
     janitor::clean_names() %>%
     select(name_birth_death, term = term_14)
-    
+
 data_cleaned <-
     data_raw %>%
-    mutate(across(name_birth_death:term, ~str_remove_all(.x, "\\[\\d{2}\\]"))) %>%
-    mutate(across(name_birth_death:term, ~str_remove_all(.x, "\\[\\d{1}\\]"))) %>%
-    mutate(across(name_birth_death:term, ~str_remove_all(.x, "\\[[[:lower:]]\\]"))) %>%
+    mutate(across(
+        name_birth_death:term,
+        ~ str_remove_all(.x, "\\[\\d{2}\\]")
+    )) %>%
+    mutate(across(
+        name_birth_death:term,
+        ~ str_remove_all(.x, "\\[\\d{1}\\]")
+    )) %>%
+    mutate(across(
+        name_birth_death:term,
+        ~ str_remove_all(.x, "\\[[[:lower:]]\\]")
+    )) %>%
     separate(name_birth_death, into = c("name", "birth_death"), sep = "\\(") %>%
-    mutate(across(birth_death, ~str_remove_all(.x, "(\\)|b\\.)"))) %>%
+    mutate(across(birth_death, ~ str_remove_all(.x, "(\\)|b\\.)"))) %>%
     separate(birth_death, into = c("birth", "death"), sep = "\\–") %>%
-    separate(term, into = c("term_start", "term_end"), sep = "\\–") %>% 
-    mutate(across(starts_with("term"), ~as.Date(.x, format = "%B %d, %Y"))) %>%
-    mutate(across(birth:death, ~ lubridate::floor_date(as.Date(.x, format = "%Y"), unit ="year")))
+    separate(term, into = c("term_start", "term_end"), sep = "\\–") %>%
+    mutate(across(starts_with("term"), ~ as.Date(.x, format = "%B %d, %Y"))) %>%
+    mutate(across(
+        birth:death,
+        ~ lubridate::floor_date(as.Date(.x, format = "%Y"), unit = "year")
+    ))
 
 presidents <-
     data_cleaned %>%
-    mutate(age = (death - birth)/lubridate::dyears(1),
-           alive = is.na(age),
-           age_in_office = (term_start - birth)/lubridate::dyears(1),
-           age_out_office = (term_end - birth)/lubridate::dyears(1),
-           end_bar = ifelse(is.na(term_end), (lubridate::today()-birth)/dyears(1), age_out_office),
-           current_age = ifelse(!is.na(age), NA, (lubridate::today() - birth)/dyears(1)))
-
-View(presidents)
+    mutate(
+        age = (death - birth) / lubridate::dyears(1),
+        alive = is.na(age),
+        age_in_office = (term_start - birth) / lubridate::dyears(1),
+        age_out_office = (term_end - birth) / lubridate::dyears(1),
+        end_bar = ifelse(is.na(term_end),
+            (lubridate::today() - birth) / dyears(1),
+            age_out_office
+        ),
+        current_age = ifelse(!is.na(age), NA,
+            (lubridate::today() - birth) / dyears(1)
+        )
+    )
 
 presidents %>%
     mutate(name = fct_reorder(name, age_in_office)) %>%
-    ggplot + aes(y = name) + 
-    geom_point(aes(x = age_in_office, color = alive), size = 3) + 
-    geom_point(aes(x = age_out_office, color = alive) , size = 3) +
-    geom_point(aes(x = current_age, color = alive), shape = 5, size = 3 )  +
-    geom_segment(aes(yend = name, x = age_in_office, xend = end_bar, color = alive), size = 3) + 
-    geom_vline(xintercept = mean(presidents$age_in_office), lty = 3) +
-    geom_vline(xintercept = mean(presidents$age_out_office, na.rm = TRUE), lty = 3) + 
-    scale_color_manual(values = c("TRUE" = "darkgreen", "FALSE" = "gray70")) + 
-    labs(x = "Age", y ="") +
+    ggplot() +
+    aes(y = name) +
+    geom_point(aes(x = age_in_office, color = alive), size = 3) +
+    geom_point(aes(x = age_out_office, color = alive), size = 3) +
+    geom_point(aes(x = current_age, color = alive), shape = 5, size = 3) +
+    geom_segment(aes(
+        yend = name,
+        x = age_in_office,
+        xend = end_bar,
+        color = alive
+    ),
+    size = 3
+    ) +
+    geom_vline(
+        xintercept = mean(presidents$age_in_office),
+        lty = 3
+    ) +
+    geom_vline(
+        xintercept = mean(presidents$age_out_office, na.rm = TRUE),
+        lty = 3
+    ) +
+    scale_color_manual(values = c("TRUE" = "darkgreen", "FALSE" = "gray70")) +
+    labs(x = "Age", y = "") +
     theme(legend.position = "none")
 
 ggsave("elections/president_ages.png", width = 8, height = 6)
