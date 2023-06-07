84f3d89b97838b6a4d4b6e1d275cc2a03b2ef286
Rob Hanssen <rob.hanssen@outlook.com>
2023-06-07 09:57:55 -0400

    linting

--- elections/scotus_ages.r
+++ elections/scotus_ages.r
@@ -10,7 +10,7 @@ theme_set(theme_light() +
         legend.position = "none"
     ))
 
-url <- "https://en.wikipedia.org/wiki/List_of_justices_of_the_Supreme_Court_of_the_United_States"
+url <- "https://en.wikipedia.org/wiki/List_of_justices_of_the_Supreme_Court_of_the_United_States" # nolint
 
 data_raw <- url %>%
     read_html() %>%
@@ -31,40 +31,40 @@ scotus <-
         term_end = str_remove_all(term_end, "\\(.*\\)"),
         across(term_start:term_end, mdy)
     ) %>%
-    mutate(birth = ifelse(birth == "born", death, birth),
-           death = ifelse(birth == death, NA, death),
-            across(birth:death, as.numeric),
-            across(birth:death, ~ymd(.x * 1e4 + 101)),
-           active = is.na(term_end),
-           alive = is.na(death),
-           term_end = as.Date(ifelse(is.na(term_end), today(), term_end), origin = "1970-01-01"),
-           age_in_office = (term_start - birth)/dyears(1),
-           age_out_office = (term_end - birth)/dyears(1),
-           term_length = (term_end - term_start)/dyears(1)
-           ) %>% 
+    mutate(
+        birth = ifelse(birth == "born", death, birth),
+        death = ifelse(birth == death, NA, death),
+        across(birth:death, as.numeric),
+        across(birth:death, ~ ymd(.x * 1e4 + 101)),
+        active = is.na(term_end),
+        alive = is.na(death),
+        term_end = as.Date(ifelse(is.na(term_end), today(), term_end),
+            origin = "1970-01-01"
+        ),
+        age_in_office = (term_start - birth) / dyears(1),
+        age_out_office = (term_end - birth) / dyears(1),
+        term_length = (term_end - term_start) / dyears(1)
+    ) %>%
     group_by(name) %>%
     mutate(
-            youngest_age_in_office = min(age_in_office),
-            oldest_age_out_office = max(age_out_office),
-            term_length = sum(term_length)
+        youngest_age_in_office = min(age_in_office),
+        oldest_age_out_office = max(age_out_office),
+        term_length = sum(term_length)
     ) %>%
     ungroup()
-
-
-#  View(scotus)
 
 scotus %>%
     mutate(name = fct_reorder(name, youngest_age_in_office)) %>%
     ggplot() +
     aes(y = name) +
-    # geom_point(aes(x = current_age, color = alive), shape = 5, size = 3) +
-    geom_segment(aes(
-        yend = name,
-        x = age_in_office,
-        xend = age_out_office,
-        color = active
-    ),
-    linewidth = 3
+    geom_segment(
+        aes(
+            yend = name,
+            x = age_in_office,
+            xend = age_out_office,
+            color = active
+        ),
+        linewidth = 3
     ) +
     scale_color_manual(values = c("TRUE" = "darkgreen", "FALSE" = "gray70")) +
     labs(x = "Age", y = "")
@@ -77,13 +77,6 @@ scotus %>%
     ggplot() +
     aes(y = name, fill = active) +
     geom_col(aes(x = term_length)) +
-    # geom_point(aes(x = current_age, color = alive), shape = 5, size = 3) +
-    # geom_segment(aes(
-    #     yend = name,
-    #     x = age_in_office,
-    #     xend = age_out_office,
-    #     color = active
-    # ),
     scale_fill_manual(values = c("TRUE" = "darkgreen", "FALSE" = "gray70")) +
     labs(x = "Age", y = "") +
     geom_vline(xintercept = median(scotus$term_length))
