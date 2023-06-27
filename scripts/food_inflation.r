76e8eaf7308a0b1e4f87e12454fc6790316d5b51
Rob Hanssen <rob.hanssen@outlook.com>
2023-06-27 12:58:31 -0400

    linting

--- scripts/food_inflation.r
+++ scripts/food_inflation.r
@@ -1,37 +1,51 @@
 library(tidyverse)
 library(lubridate)
-theme_set(theme_light())
-
-my_theme <- theme(
-    plot.title.position = "plot",
-    panel.grid.minor = element_blank(),
-    plot.caption.position = "plot",
-    plot.caption = element_text(hjust = 0)
-)
+theme_set(theme_light() +
+    theme(
+        plot.title.position = "plot",
+        panel.grid.minor = element_blank(),
+        plot.caption.position = "plot",
+        plot.caption = element_text(hjust = 0)
+    ))
 
 source("functions.r")
 
+# CUSR0000SAF11: Consumer Price Index for All Urban Consumers: Food at Home in U.S. City Average    # nolint
+# CUSR0000SEFV: Consumer Price Index for All Urban Consumers: Food Away from Home in U.S. City Average # nolint
+# CUSR0000SAF113: Consumer Price Index for All Urban Consumers: Fruits and Vegetables in U.S. City Average # nolint
 
-# CUSR0000SAF11: Consumer Price Index for All Urban Consumers: Food at Home in U.S. City Average
-# CUSR0000SEFV: Consumer Price Index for All Urban Consumers: Food Away from Home in U.S. City Average
-# CUSR0000SAF113: Consumer Price Index for All Urban Consumers: Fruits and Vegetables in U.S. City Average
 
-
-inflation <- retrieve_data(index = c("CUSR0000SAF11", "CUSR0000SEFV", "CUSR0000SAF113")) %>%
+inflation <- retrieve_data(index = c(
+    "CUSR0000SAF11",
+    "CUSR0000SEFV",
+    "CUSR0000SAF113"
+)) %>%
     pivot_wider(names_from = index, values_from = value) %>%
     drop_na() %>%
-    rename(food_at_home = CUSR0000SAF11, food_out = CUSR0000SEFV, food_fruitveg = CUSR0000SAF113) %>%
+    rename(
+        food_at_home = CUSR0000SAF11,
+        food_out = CUSR0000SEFV,
+        food_fruitveg = CUSR0000SAF113
+    ) %>%
     mutate(month = month(date), year = year(date)) %>%
     group_by(month) %>%
-    mutate(across(2:4, ~ .x / lag(.x) - 1, .names = "{col}_annualinflation")) %>%
+    mutate(across(2:4, ~ .x / lag(.x) - 1,
+        .names = "{col}_annualinflation"
+    )) %>%
     ungroup()
 
 plot_foodinflation <- inflation %>%
     filter(date > ymd(20180101)) %>%
     ggplot() +
-    geom_line(aes(x = date, y = food_at_home_annualinflation), color = "blue") +
-    geom_line(aes(x = date, y = food_fruitveg_annualinflation), color = "darkgreen") +
-    geom_line(aes(x = date, y = food_out_annualinflation), color = "maroon") +
+    geom_line(aes(x = date, y = food_at_home_annualinflation),
+        color = "blue"
+    ) +
+    geom_line(aes(x = date, y = food_fruitveg_annualinflation),
+        color = "darkgreen"
+    ) +
+    geom_line(aes(x = date, y = food_out_annualinflation),
+        color = "maroon"
+    ) +
     scale_y_continuous(labels = scales::percent_format(), breaks = 0:100 * .02)
 
 
@@ -73,9 +87,8 @@ plot_foodhome <-
     labs(
         x = "", y = "Food price index",
         title = "Indexed food-at-home prices",
-        caption = "Source: FRED CUSR0000SAF11\nIndexed to the average full-year 2014-2018"
-    ) +
-    my_theme
+        caption = "Source: FRED CUSR0000SAF11\nIndexed to the average full-year 2014-2018" # nolint
+    )
 
 ggsave("graphs/food_inflation.png",
     width = 6, height = 4,
@@ -91,4 +104,7 @@ plot_foodout <-
     filter(date > ymd(20120101)) %>%
     ggplot() +
     geom_line(aes(x = date, y = food_out_indexed), color = "maroon") +
-    scale_y_continuous(labels = scales::dollar_format(), breaks = 1 + -100:100 * .05)
+    scale_y_continuous(
+        labels = scales::dollar_format(),
+        breaks = 1 + -100:100 * .05
+    )
