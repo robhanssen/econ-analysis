2fb93c50e24f6a1ab8803e8fa05e2f8ea5690ff0
Rob Hanssen <rob.hanssen@outlook.com>
2023-08-19 17:59:32 -0400

    cpi adjusted, per person

--- dev/savings.r
+++ dev/savings.r
@@ -10,6 +10,7 @@ library(lubridate)
 
 theme_set(theme_light() +
     theme(
+        plot.title.position = "plot",
         plot.caption = element_text(hjust = 0),
         plot.caption.position = "plot",
         panel.grid.minor = element_blank()
@@ -112,7 +113,7 @@ ggsave("dev/savings_sum.png",
     plot = p
 )
 
-cpi <- 
+cpi <-
     retrieve_data("CPIAUCSL") %>%
     pivot_wider(names_from = "index") %>%
     janitor::clean_names() %>%
@@ -198,9 +199,52 @@ excess_savings_g_cpi <-
     )
 
 
-p2 <- (excess_savings_g + excess_savings_g_cpi) / (savings_g + savings_g_cpi) 
+p2 <- (excess_savings_g + excess_savings_g_cpi) / (savings_g + savings_g_cpi)
 
 ggsave("dev/savings_sum_cpi.png",
     width = 12, height = 8,
     plot = p2
 )
+
+#
+# by person
+#
+#
+
+savingspop_dat <-
+    retrieve_data(
+        indexes = c("PMSAVE", "POPTOTUSA647NWDB", "CPIAUCSL"),
+        "FRED"
+    ) %>%
+    pivot_wider(names_from = "index", values_from = "value") %>%
+    arrange(date) %>%
+    janitor::clean_names() %>%
+    mutate(
+        pop = zoo::na.approx(poptotusa647nwdb, na.rm = FALSE),
+        cpi = zoo::na.approx(cpiaucsl, na.rm = FALSE),
+        cpi_index = cpi[.$date == ymd(20160101)],
+        pmsave = pmsave / 12
+    ) %>%
+    fill(pop, .direction = "downup") %>%
+    fill(cpi, .direction = "downup") %>%
+    mutate(
+        pm_save_by_person = pmsave * 1e9 / pop,
+        pm_save_by_person_cpi = pm_save_by_person * cpi_index / cpi
+    )
+
+savingspop_dat %>%
+    filter(date >= ymd(20160101)) %>%
+    ggplot(aes(date)) +
+    geom_line(color = "darkgreen", aes(y = pm_save_by_person_cpi)) +
+    # geom_line(color = "red", aes(y = pm_save_by_person)) +
+    coord_cartesian(ylim = c(0, NA)) +
+    scale_x_date(date_minor_breaks = "1 year") +
+    scale_y_continuous(
+        labels = scales::dollar_format(),
+        breaks = seq(0, 2000, 200),
+    ) +
+    labs(
+        x = "", y = "Personal savings rate per person per month (in 2016 $)",
+        caption = "Source: FRED PMSAVE, POPTOTUSA647NWDB, CPIAUCSL",
+        title = "CPI-adjusted savings per person per month"
+    )
