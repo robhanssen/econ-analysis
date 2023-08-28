30f3335f6679d13c143bf0069d46aea8f9e160bc
Rob Hanssen <rob.hanssen@outlook.com>
2023-08-28 10:14:33 -0400

    final updates

--- dev/cc_debt.r
+++ dev/cc_debt.r
@@ -1,4 +1,16 @@
 library(tidyverse)
+library(patchwork)
+
+theme_set(
+    theme_light() +
+        theme(
+            panel.grid.minor.y = element_blank(),
+            plot.title.position = "plot",
+            plot.title = element_text(),
+            plot.caption.position = "plot",
+            plot.caption = element_text(hjust = 0)
+        )
+)
 
 source("functions.r")
 
@@ -11,6 +23,8 @@ raw_data <-
             "GDP"
         )
     )
+
+cpi_year <- "2012"
 
 ccdebt_processed <-
     raw_data %>%
@@ -27,7 +41,7 @@ ccdebt_processed <-
         ~ zoo::na.approx(.x, na.rm = FALSE)
     )) %>%
     fill(c("cpi", "pop", "gdp"), .direction = "downup") %>%
-    filter(date > ymd(20120101)) %>%
+    filter(date >= ymd(glue::glue("{cpi_year}-01-01"))) %>%
     mutate(
         ccdebt_cpi = ccdebt * first(cpi) / cpi,
         ccdebt_cpi_pop = ccdebt_cpi * first(pop) / pop,
@@ -39,21 +53,96 @@ long_format_data <-
     ccdebt_processed %>%
     pivot_longer(starts_with("ccdebt"),
         names_to = "index",
-        values_to = "value")
+        values_to = "value"
+    )
 
-labels <- 
+labels <-
     long_format_data %>%
     slice_max(date, by = index) %>%
     select(date, value, index)
 
-long_format_data %>%
+cc_debt_g <-
+    long_format_data %>%
     ggplot(aes(x = date, y = value, color = index)) +
     geom_line() +
-    scale_y_continuous(limits = c(400, NA)) +
-    geom_vline(xintercept = inaugdates, lty = 2, alpha = .5) +
+    scale_x_date(
+        date_labels = "%Y",
+        date_breaks = "4 years",
+        date_minor_breaks = "1 year"
+    ) +
+    scale_y_continuous(
+        limits = c(500, NA),
+        labels = scales::dollar_format(scale = 1, suffix = " B")
+    ) +
     ggrepel::geom_label_repel(
         data = labels,
         hjust = 1,
         aes(x = date, y = value, label = index)
-        ) + 
+    ) +
+    labs(
+        title = glue::glue("(Adjusted) credit card debt growth between {cpi_year} and {year(today())}"),
+        x = "", y = "Adjusted credit-card debt (in $)",
+        caption = glue::glue(
+            "Source: FRED CCLACBW027SBOG and CPIAUCSL\n",
+            "CPI-adjusted amounts in {cpi_year} dollars"
+        )
+    ) +
     theme(legend.position = "none")
+
+av2019 <- long_format_data %>%
+    filter(index == "ccdebt_cpi_pop", year(date) == 2019) %>%
+    summarize(av = mean(value)) %>%
+    pull(av)
+
+av2019_line <-
+    tibble(x = ymd(20190101, 20240101),
+           y = av2019)
+
+pred2022onw <- long_format_data %>%
+    filter(index == "ccdebt_cpi_pop", year(date) >= 2022) %>%
+    lm(value ~ date, data = .) %>%
+    broom::augment(
+        newdata =
+            tibble(date = seq(
+                ymd(20220101), today() + years(1), "1 month"
+            ))
+    )
+
+xdate <- as.Date(
+    approx(pred2022onw$.fitted, pred2022onw$date, xout = av2019)$y,
+    origin = "1970-01-01"
+)
+
+xdate_g <-
+    cc_debt_g +
+    geom_line(
+        data = pred2022onw,
+        aes(x = date, y = .fitted),
+        inherit.aes = FALSE,
+        alpha = .25,
+        color = "gray70",
+        linewidth = 2
+    ) +
+    geom_line(
+        inherit.aes = FALSE,
+        data = av2019_line,
+        aes(x, y),
+        alpha = .25,
+        color = "gray70",
+        linewidth = 2
+        ) +
+    geom_vline(
+        xintercept = xdate,
+        alpha = .25,
+        color = "gray70",
+        linewidth = 2
+    ) +
+    annotate("text", x = xdate, y = 520, hjust = 1,
+            size = 2,
+            label = format(xdate, format = "%b %d,\n%Y"))
+
+
+ggsave("graphs/ccdebt.png",
+    width = 12, height = 5,
+    plot = cc_debt_g + xdate_g
+)
