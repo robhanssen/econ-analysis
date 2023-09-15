3a095787f871f662d9978f6caafe5f0ee724c635
Rob Hanssen <rob.hanssen@outlook.com>
2023-09-15 15:20:36 -0400

    generate website from figures

--- genweb.r
+++ genweb.r
@@ -1,11 +1,18 @@
 images <- list.files(path = "graphs/", pattern = ".*\\.png$", full.names = TRUE)
-links <- paste0("<p><img src=\"", images, "\" width = \"50%\"></p><hr>")
+
+links <- paste0(
+    "<p><img src=\"",
+    images,
+    "\" width = \"50%\" alt =\"",
+    stringr::str_remove_all(images, "(^.*/|\\.png$)"),
+    "\"></p><hr/>", collapse = ""
+)
 
 
 header <-
     paste0(
-        "<html><title>Econ Analysis</title>
-        <body>"
+        "<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'>",
+        "<html><title>Econ Analysis</title><body>"
     )
 
 footer <-
@@ -14,7 +21,7 @@ footer <-
     )
 
 fullpage <-
-    paste0(header, paste0(links, collapse = ""), footer, collapse = "")
+    paste0(header, links, footer, collapse = "")
 
 
 writeLines(fullpage, con = "econ-analysis.html")
