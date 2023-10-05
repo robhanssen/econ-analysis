5b48c7a142842b8a49e3ab04a832f566ad2ebb64
Rob Hanssen <rob.hanssen@outlook.com>
2023-10-05 08:40:29 -0400

    pattern change

--- run-all.r
+++ run-all.r
@@ -1,6 +1,6 @@
 library(purrr)
 
-walk(list.files(path = "./scripts/", pattern = "*.r", full.names = TRUE),
+walk(list.files(path = "./scripts/", pattern = "*.r$", full.names = TRUE),
     source)
 
 source("genweb.r")
