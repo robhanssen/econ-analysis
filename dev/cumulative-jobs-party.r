c3179692083981ca88c915afee8394cc0f88fb65
Rob Hanssen <rob.hanssen@outlook.com>
2023-10-06 14:17:52 -0400

    initial commit, based on facebook comment

---
+++ dev/cumulative-jobs-party.r
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(lubridate)
+library(broom)
+library(patchwork)
+theme_set(theme_light())
+
+source("functions.r")
+
+jobs <- retrieve_data("PAYEMS", "FRED") %>%
+    mutate(
+        year = year(date),
+        month = month(date)
+    ) %>%
+    arrange(date) %>%
+    mutate(value = value * 1000) %>%
+    mutate(growth_month = value - lag(value)) %>%
+    group_by(month) %>%
+    mutate(growth_annual = value - lag(value)) %>%
+    ungroup() %>%
+    mutate(prezpd = cut(date, breaks = inaugdates)) %>%
+    mutate(prez = as.character(prezpd)) %>%
+    inner_join(presidentinfo %>% mutate(prez = as.character(inaugdate)))
+
+jobs %>%
+    filter(date > ymd(19891231)) %>%
+    group_by(party) %>%
+    mutate(party_growth = cumsum(growth_month)) %>%
+    ggplot(aes(x = date, y = party_growth, color = party)) + 
+    geom_point(shape = 1, alpha = .3) + 
+    geom_line(linetype = 2) +
+    geom_hline(yintercept = 0, linewidth = 2, alpha = .8) + 
+    scale_y_continuous(
+        labels = scales::label_number()
+    ) +
+    scale_color_manual(values = c("R" = "red", "D" = "blue")) + 
+    labs(x = "", y = "Cumulative jobs growth by president party", 
+        caption = "Source: FRED PAYEMS data")
+
+ggsave("dev/jobs_growth_by_party.png", width = 7, height = 5)
