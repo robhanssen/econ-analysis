35b0eeb04048198a92f14e8e15e90f6fe323f43b
Rob Hanssen <rob.hanssen@outlook.com>
2024-10-04 14:13:38 -0400

    safe run enabled

--- run-all.r
+++ run-all.r
@@ -1,8 +1,10 @@
 library(purrr)
 
+safe_source <- safely(source, quiet = FALSE)
+
 walk(
     list.files(path = "./scripts/", pattern = "*.r$", full.names = TRUE),
-    source
+    safe_source
 )
 
 source("genweb.r")
