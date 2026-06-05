1e9054a3b871e38c514c9b48b1b8f009e7b72f5c
Rob Hanssen <rob.hanssen@outlook.com>
2026-06-05 15:43:40 -0400

    replace quantmod function with api call

--- functions.r
+++ functions.r
@@ -1,14 +1,34 @@
 options("getSymbols.warning4.0" = FALSE)
 
 
+# get_index <- function(index = "GDP", src = "FRED") {
+#     t <- quantmod::getSymbols(index, src = src, auto.assign = FALSE)
+#     tibble::tibble(
+#         date = zoo::index(t),
+#         index = index,
+#         value = as.numeric(t)
+#     )
+# }
+
+BASE_API_URL <- paste0("https://api.stlouisfed.org/fred/series/observations?series_id=##IDX##&api_key=", fred_api_key, "&file_type=json")
+
 get_index <- function(index = "GDP", src = "FRED") {
-    t <- quantmod::getSymbols(index, src = src, auto.assign = FALSE)
-    tibble::tibble(
-        date = zoo::index(t),
-        index = index,
-        value = as.numeric(t)
-    )
+    if (src == "FRED") {
+        url <- gsub("##IDX##", index, BASE_API_URL)
+        data <- jsonlite::fromJSON(url)$observations
+        data <- data %>%
+            mutate(
+                index = index,
+                date = ymd(date),
+                value = as.numeric(value),
+                .keep = "none"
+            )
+    }
+    as_tibble(data)
 }
+
+
+
 
 retrieve_data <- function(indexes = c("GDP", "GDPC1"), src = "FRED") {
     purrr::map_df(indexes, ~ get_index(.x, src))
